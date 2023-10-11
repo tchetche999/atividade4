1a9f03518f592494ba78e1755d9426d635b34e40
deborasilvaaaa <deborasilvacardosos021@gmail.com>
2023-10-11 11:10:23 -0300

    adicionei o codigo

---
+++ chadissonatividade4.py
@@ -0,0 +1,6 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+print('descubra o sucessor e o antecessor de um múmero qualquer')
+numero = float(input('digite um número: '))
+sucessor = numero + 1 
+antecessor = numero - 1
+print(f'o sucessor do seu número é {sucessor} e o antecessor é {antecessor}')
